72661adcb33cbd353f84c97d7c579349f35da1ab
M <monacuaulma@gmail.com>
2023-05-19 21:36:08 -0400

    Fixed subschema

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
-const projectSchema = require("../models/Project");
+
+const projectSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
 const userSchema = mongoose.Schema({
 	username: {
