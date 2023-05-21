6809e3b0e83bfc6520502abd58bb5a062ac3b1dd
M <monacuaulma@gmail.com>
2023-05-21 12:01:17 -0400

    Fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userSchema = require("../models/User");
-const bcrypt = require("bcrypt");
+
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
